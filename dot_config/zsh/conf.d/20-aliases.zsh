cf32bff756c257e2b067acddf1a89e2023713f21
Keisuke Shimokawa <shimoke4869@gmail.com>
2026-09-26 00:36:02 +0900

    ホーム側の設定変更を反映（mise に uv を追加、モデル・エイリアス設定の更新）

--- dot_config/zsh/conf.d/20-aliases.zsh
+++ dot_config/zsh/conf.d/20-aliases.zsh
@@ -9,3 +9,12 @@ alias ls='eza'
 alias ll='eza -l --git'
 alias la='eza -la --git'
 alias lt='eza --tree'
+
+# エイリアスを張るのが定番(~/.zshrc に)
+alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
+
+alias zed='mise exec -- zed'
+
+glow90() {
+    glow -w $(( COLUMNS * 9 / 10 )) "$@"
+}
